c1731a612ae8d5c59cf8fa45f10729e4abd1b9d4
Gerardo Flores <gerardo_flores@mckinsey.com>
2013-10-13 00:55:41 -0500

    added nlog logging

--- Server/AppHost.cs
+++ Server/AppHost.cs
@@ -2,15 +2,27 @@
 using SimpleServiceStack.Models;
 using Funq;
 using ServiceStack.Razor;
+using ServiceStack.OrmLite;
+using ServiceStack.OrmLite.SqlServer;
+using ServiceStack.CacheAccess;
+using ServiceStack.CacheAccess.Providers;
+using ServiceStack.Logging;
+using ServiceStack.Logging.NLogger;
 
 namespace SimpleServiceStack
 {
     public class AppHost : AppHostBase
     {
+        private static ILog logger;
+
         /// <summary>
         /// Initializes a new instance of your ServiceStack application, with the specified name and assembly containing the services.
         /// </summary>
-        public AppHost() : base("Player Web Service", typeof(PlayerService).Assembly) { }
+        public AppHost() : base("Player Web Service", typeof(PlayerService).Assembly) 
+        {
+            LogManager.LogFactory = new NLogFactory();
+            logger = LogManager.GetLogger(typeof(AppHost));
+        }
 
         /// <summary>
         /// Configure the container with the necessary routes for your ServiceStack application.
@@ -20,6 +32,13 @@ namespace SimpleServiceStack
         {
             //loading Razor plugin to load the client/index.html file as the main file
             Plugins.Add(new RazorFormat());
+
+            container.Register<IDbConnectionFactory>(new OrmLiteConnectionFactory("sqlServer", SqlServerDialect.Provider));
+
+            //The MemoryCacheClient is a great way to get started with caching; nothing external to setup.
+            container.Register<ICacheClient>(c => new MemoryCacheClient());
+
+            logger.Info("The AppHost has been parsed and started");
         }
     }
 }
